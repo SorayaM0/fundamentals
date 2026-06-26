a660d7da0347511af9b9458b25769670f35fbcf5
Soraya Mosavi <sorayamosavi36@gmail.com>
2026-06-26 16:39:40 -0400

    Updates

---
+++ Number_Guessing/Number_Guessing/Program.cs
@@ -0,0 +1,43 @@
+﻿Random random= new Random(); 
+bool playAgain= true; 
+int min=1;
+int max=100; 
+int guess; 
+int number; 
+int guesses; 
+string response; 
+
+while (playAgain)
+{
+    guess = 0 ; 
+    guesses=0 ; 
+    number=random.Next(min, max); 
+while (guess!= number)
+    {
+        System.Console.WriteLine("Guess a number between" + min + "-"+ max + ":");
+        guess= Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Guess:"+ guess);
+
+
+        if(guess > number)
+        {
+            System.Console.WriteLine(guess +" is too high");
+        }
+       else if ( guess < number)
+        {
+            System.Console.WriteLine(guess+ "is too low");
+        }
+        guesses++ ; 
+    }  
+    System.Console.WriteLine("Number:" + number);
+    System.Console.WriteLine("You Win!");
+    System.Console.WriteLine("quesses:"+ guesses);
+    System.Console.WriteLine(" would you like to play again? (Y/N)");
+    response= Console.ReadLine(); 
+    if (response="Y")
+    {
+        playAgain=true; 
+    }
+    else if (response="N"); 
+    playAgain= false;
+}
