9aa8517a347c9248f9000895cae77a6f1d68f706
Soraya Mosavi <sorayamosavi36@gmail.com>
2026-06-26 14:48:34 -0400

    Add calculator project

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, welcome to calculator program");
+Console.WriteLine("Please enter your first number");  
+int num1= Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Please enter your second number");
+int num2= Convert.ToInt32(Console.ReadLine()); 
+System.Console.WriteLine("What type of operation do you want?");
+System.Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication and d for division");
+
+string answer= Console.ReadLine(); 
+
+if(answer == "a")
+{
+  System.Console.WriteLine("resunt:" + (num1 +num2));
+}
+else if (answer == "s" )
+{
+    System.Console.WriteLine("resunt:" + (num1 - num2));
+}
+else if (answer == "m" )
+{
+    System.Console.WriteLine( "resunt:" + (num1 * num2) );
+}
+else if (answer == "d" )
+{
+    System.Console.WriteLine("resunt:" + (num1/ num2));
+}
---
+++ Dice_Game/Dice_Game/Program.cs
@@ -0,0 +1,40 @@
+﻿int playerRandomNum;
+int enemeyRandomNum;
+int playerPoint=0; 
+int enemyPoint=0 ; 
+Random random = new Random();
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Press any key to roll the dice!");
+    Console.ReadKey();
+
+    playerRandomNum = random.Next(1, 7);
+    Console.WriteLine("You rolled : " + playerRandomNum);
+
+    System.Console.WriteLine(".....");
+    System.Threading.Thread.Sleep(1000);
+
+
+
+    enemeyRandomNum = random.Next(1, 7);
+    System.Console.WriteLine("Enemy rolled:" + enemeyRandomNum);
+
+    if (playerRandomNum>enemeyRandomNum)
+    {
+        playerPoint++; 
+        System.Console.WriteLine("Player wins this round");
+    }
+    else if ( playerRandomNum<enemeyRandomNum)
+    {
+        enemyPoint++; 
+        System.Console.WriteLine("enemy wins this round");
+    }
+    else if (playerRandomNum==enemeyRandomNum)
+    {
+        System.Console.WriteLine("Both are equal, so no one wins ");
+    }
+    System.Console.WriteLine("The score of player is :" + playerPoint + "The score of enemy is :"+ enemyPoint);
+    System.Console.WriteLine();
+}
+
